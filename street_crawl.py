f774a8d8939681dfe595d5a36f4583a635863673
Dylan <d_h@mit.edu>
2020-11-19 13:40:31 -0600

    changing to walking directions

--- street_crawl.py
+++ street_crawl.py
@@ -1,7 +1,8 @@
 import sys
 from utils import *
-from API_KEYS import API_KEY_DIRECTIONS, API_KEY_STREETVIEW
-
+# from API_KEYS import API_KEY_DIRECTIONS, API_KEY_STREETVIEW
+API_KEY_DIRECTIONS = # your api key
+API_KEY_STREETVIEW = # your api key
 '''Google Street View Movie Maker
 
 Usage is:
@@ -9,7 +10,7 @@ Usage is:
 
 
 For example, to make a one-second video of the entrance of Joshua Treet National Park:
-	python2 ./street_crawl.py 33.669793 -115.802125 33.671796 -115.801851 joshua_tree
+	python2 ./street_crawl.py 33.669793 -115.802125 33.671796 -115.801851 joshua_tree "640x320"
 
 Note: usage requires your own API keys.
 
@@ -19,10 +20,12 @@ def main(lat_lon_A, lat_lon_B, filestem, picsize):
 	print "Tracing path from ({0}) to ({1})".format(lat_lon_A, lat_lon_B)
 	# Request driving directions from A to B
 	gd = googlemaps.Client(key=API_KEY_DIRECTIONS)
-	directions_result = gd.directions(origin=lat_lon_A, destination=lat_lon_B, mode="driving")
+	directions_result = gd.directions(origin=lat_lon_A, destination=lat_lon_B, mode="walking")
 	# Convert driving directions into sequence of GPS points
+	# print(directions_result)
 	path_points = polyline.decode(directions_result[0]['overview_polyline']['points'])
 	dense_points = [interpolate_points(pt[0],pt[1],hop_size=10) for pt in zip(path_points[:-1],path_points[1:])]	
+	print(path_points)
 	look_points_rough = [item for sequence in dense_points for item in sequence]
 	# Remove unnecessary points
 	look_points = clean_look_points(look_points_rough)
@@ -31,6 +34,7 @@ def main(lat_lon_A, lat_lon_B, filestem, picsize):
 	if continue_opt not in ['Yes','yes']:
 		return
 	# Download sequence of images (up to a limit? What's the limit in a day?)
+	download_images_for_path(API_KEY_STREETVIEW, filestem, look_points)
 	# download_images_for_path(API_KEY_STREETVIEW, filestem, look_points, picsize=picsize)
 	# Assign images new filenames (and remove bad images)
 	line_up_files(filestem, new_dir="./lineup-{0}/".format(filestem))
